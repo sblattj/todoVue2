c61d1a2878eef3a50c96c812995d865cac24898c
sblatt <sblatt@centricsoftware.com>
2019-06-01 05:13:23 -0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="bg-primary text-white text-center p-2">{{name}}'s To do App</h1>
+    <div class="container-fluid p-4">
+      <div class="class row" v-if="filteredTasks.length == 0">
+        <div class="col text-center"></div>
+             <b>Nothing to do</b>
+        </div>
+      <template v-else>
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="class col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <!-- <div class="row" v-for="t in tasks" v-bind:key="t.action"> -->
+        <div class="col">{{t.action}}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="formg-check-input">
+          {{t.done}}
+        </div>
+      </div>
+      </template>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control">
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      name: "Stephen",
+      tasks: [],
+        hideCompleted: true,
+        newItemText: ""
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+    }
+  },
+  methods: {
+    addNewTodo() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false
+      });
+      this.storeData()
+      this.newItemText = "";
+    },
+    storeData() {
+      localStorage.setItem("todos", JSON.stringify(this.tasks));
+    },
+    deleteCompleted() {
+      this.tasks = this.tasks.filter(t => !t.done);
+      this.storeData();
+    }
+  },
+  created() {
+    let data = localStorage.getItem("todos");
+    if (data !== null) {
+      this.tasks = JSON.parse(data);
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
